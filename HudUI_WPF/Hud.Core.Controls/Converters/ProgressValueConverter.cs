64210b6d170a3c06710e445a4999616f09a8bd7a
hbm <hbmin3789@naver.com>
2021-01-15 17:59:58 +0900

    feat : add progress bar add progress box add converter

---
+++ HudUI_WPF/Hud.Core.Controls/Converters/ProgressValueConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace Hud.Core.Controls.Converters
+{
+    public class ProgressValueConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int retval = (int)value;
+            return ((retval / 10) * 10);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
