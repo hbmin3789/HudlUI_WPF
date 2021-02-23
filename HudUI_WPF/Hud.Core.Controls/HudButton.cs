947a9292d97a78a80cee85536b4959f4c6bb7190
hbmin3789 <hbmin3789@naver.com>
2021-02-23 15:55:49 +0900

    feat : Hud Button add

---
+++ HudUI_WPF/Hud.Core.Controls/HudButton.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace Hud.Core.Controls
+{
+    public class HudButton : Button
+    {
+        public static DependencyProperty HOverBackgroundProperty = 
+            DependencyProperty.Register("", typeof(Brush),
+                                        typeof(HudButton), 
+                                        new FrameworkPropertyMetadata(
+                                            new SolidColorBrush(Color.FromRgb(0xb2, 0xeb, 0xf2)),
+                                            FrameworkPropertyMetadataOptions.BindsTwoWayByDefault));
+
+    }
+}
--- HudUI_WPF/HudUI_WPF_Test/MainWindow.xaml.cs
+++ HudUI_WPF/HudUI_WPF_Test/MainWindow.xaml.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Prism.Mvvm;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,14 +17,44 @@ using System.Windows.Shapes;
 
 namespace HudUI_WPF_Test
 {
+    public class Person : BindableBase
+    {
+        private string _name;
+        public string Name
+        {
+            get => _name;
+            set => SetProperty(ref _name, value);
+        }
+    }
+
+    public class ViewModel : BindableBase
+    {
+        private ObservableCollection<Person> _personList = new ObservableCollection<Person>();
+        public ObservableCollection<Person> PersonList
+        {
+            get => _personList;
+            set => SetProperty(ref _personList, value);
+        }
+
+        public ViewModel()
+        {
+            for(int i = 0; i < 20; i++)
+            {
+                PersonList.Add(new Person() { Name = "person" + i.ToString() });
+            }
+        }
+    }
+
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
     {
+
         public MainWindow()
         {
             InitializeComponent();
+            this.DataContext = new ViewModel();
         }
     }
 }
